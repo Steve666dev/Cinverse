8f0ec418df7a93b396b52974326a19a2c1fabef3
steve <steveharigston666@gmail.com>
2026-09-10 10:14:42 +0530

    Update encrypt-keys.js

--- encrypt-keys.js
+++ encrypt-keys.js
@@ -2,8 +2,8 @@ import CryptoJS from 'crypto-js';
 
 const SECRET_PHRASE = 'CINEMATIX_SECURE_KEY_2026';
 
-const omdb = 'a7d0a9ca';
-const tmdb = '2cc076f8b37cd3cfe47c4f528f5e7fc0';
+const omdb = 'API';
+const tmdb = 'API';
 
 const encOmdb = CryptoJS.AES.encrypt(omdb, SECRET_PHRASE).toString();
 const encTmdb = CryptoJS.AES.encrypt(tmdb, SECRET_PHRASE).toString();
