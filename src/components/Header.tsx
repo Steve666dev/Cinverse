47def853fc1d5f0904c92ae0616956150ae463df
Cineverse Developer <user@example.com>
2026-09-14 12:04:23 +0530

    Add TextScramble effect to CINEVERSE logo in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
+import { TextScramble } from './core/text-scramble';
 import { useWatchlist } from '../context/WatchlistContext';
 import { Dock, DockItem, DockIcon, DockLabel } from './core/dock';
 import { Magnetic } from '@/components/core/magnetic';
@@ -119,7 +120,9 @@ const Header: React.FC<HeaderProps> = ({ onSearch, onDiscover }) => {
           range={160}
         >
           <button className="logo" onClick={handleLogoClick}>
-            <span className="logo-text">CINEVERSE</span>
+            <span className="logo-text">
+              <TextScramble>CINEVERSE</TextScramble>
+            </span>
           </button>
         </Magnetic>
       <nav className="links">
